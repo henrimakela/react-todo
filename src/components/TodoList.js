ef89eeb96940ca83772cf2343402a21b0b403733
Sampsa <sampsa.isohatala@gmail.com>
2020-10-14 16:32:13 +0300

    Added all the basic functionality

---
+++ src/components/TodoList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../App.css';
+import Todo from './Todo'
+
+const TodoList = (props) => {
+    return(
+        <div className="todo-container">
+            <ul className="todo-list">
+                {
+                    props.todos.map(todo => (
+                        <Todo key={todo.id} todo={todo} text={todo.text} todos={props.todos} setTodos={props.setTodos}/>
+                    ))
+                }
+            </ul>
+        </div>
+    )
+}
+
+export default TodoList
